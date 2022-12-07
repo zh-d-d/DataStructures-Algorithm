6cb43cebfb3fb6f014c6b87d35099493b19c21dc
zhangdd <86431843@qq.com>
2022-12-07 14:57:48 +0800

    feat:104. 二叉树的最大深度
    Signed-off-by: zhangdd <86431843@qq.com>

---
+++ leetcode/src/main/java/com/zhangdd/leetcode/problem0104/Solution104.java
@@ -0,0 +1,38 @@
+package com.zhangdd.leetcode.problem0104;
+
+/**
+ * @author zhangdd on 2022/12/7
+ * <a href="https://leetcode.cn/problems/maximum-depth-of-binary-tree/">题目描述</a>
+ */
+public class Solution104 {
+
+
+    public int maxDepth(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
