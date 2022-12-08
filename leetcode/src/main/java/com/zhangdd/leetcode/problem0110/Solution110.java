05566b0c007a0bee116074d4500f08af7026abb3
zhangdd <86431843@qq.com>
2022-12-08 14:07:25 +0800

    feat:110. 平衡二叉树
    Signed-off-by: zhangdd <86431843@qq.com>

---
+++ leetcode/src/main/java/com/zhangdd/leetcode/problem0110/Solution110.java
@@ -0,0 +1,52 @@
+package com.zhangdd.leetcode.problem0110;
+
+/**
+ * @author zhangdd on 2022/12/8
+ * <a href="https://leetcode.cn/problems/balanced-binary-tree/discussion/">题目描述</a>
+ */
+public class Solution110 {
+
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
