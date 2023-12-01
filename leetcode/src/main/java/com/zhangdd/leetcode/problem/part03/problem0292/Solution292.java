bbc98ac4e694bca0268d84ef3ecaaf9f57483058
zhangdd <86431843@qq.com>
2023-12-01 13:56:45 +0800

    feat:292. Nim 游戏
    Signed-off-by: zhangdd <86431843@qq.com>

---
+++ leetcode/src/main/java/com/zhangdd/leetcode/problem/part03/problem0292/Solution292.java
@@ -0,0 +1,13 @@
+package com.zhangdd.leetcode.problem.part03.problem0292;
+
+/**
+ * @author zhangdd on 2023/1/13
+ *
+ * <a href="https://leetcode.cn/problems/nim-game/">题目描述</a>
+ */
+public class Solution292 {
+
+    public boolean canWinNim(int n) {
+        return n%4 != 0;
+    }
+}
