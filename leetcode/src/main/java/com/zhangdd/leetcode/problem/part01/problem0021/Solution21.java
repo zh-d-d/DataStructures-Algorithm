40d2bf88063a78fa3276cf73ace72872435fcd64
zhangdd <86431843@qq.com>
2023-12-25 22:47:21 +0800

    refactor:21
    Signed-off-by: zhangdd <86431843@qq.com>

--- leetcode/src/main/java/com/zhangdd/leetcode/problem/part01/problem0021/Solution21.java
+++ leetcode/src/main/java/com/zhangdd/leetcode/problem/part01/problem0021/Solution21.java
@@ -7,6 +7,38 @@ package com.zhangdd.leetcode.problem.part01.problem0021;
 public class Solution21 {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode result = new ListNode();
+//        //同时为空 直接返回就好了
+//        if (null == list1 && null == list2) {
+//            return null;
+//        }
+//        //其中一个为空 ，直接将不为空的后面的内容串过来 返回就好了
+//        if (null == list1) {
+//            result.val = list2.val;
+//            result.next = list2.next;
+//            return result;
+//        }
+//        if (null == list2) {
+//            result.val = list1.val;
+//            result.next = list1.next;
+//            return result;
+//        }
+//
+//        //两个都不为空，取一个小的节点，同时移动对应的传
+//        int val1 = list1.val;
+//        int val2 = list2.val;
+//        if (val1 <= val2) {
+//            result.val = val1;
+//            list1 = list1.next;
+//            result.next = mergeTwoLists(list1, list2);
+//        } else {
+//            result.val = val2;
+//            list2 = list2.next;
+//            result.next = mergeTwoLists(list1, list2);
+//        }
+
+
+        //优化
         ListNode result = new ListNode();
         //同时为空 直接返回就好了
         if (null == list1 && null == list2) {
@@ -14,29 +46,34 @@ public class Solution21 {
         }
         //其中一个为空 ，直接将不为空的后面的内容串过来 返回就好了
         if (null == list1) {
-            result.val = list2.val;
-            result.next = list2.next;
-            return result;
+            result.next = list2;
+            return result.next;
         }
         if (null == list2) {
-            result.val = list1.val;
-            result.next = list1.next;
-            return result;
+            result.next = list1;
+            return result.next;
+        }
+        ListNode tmp = result;
+        while (null != list1 && null != list2) {
+            int val1 = list1.val;
+            int val2 = list2.val;
+            if (val1 <= val2) {
+                tmp.next = list1;
+                list1 = list1.next;
+            } else {
+                tmp.next = list2;
+                list2 = list2.next;
+            }
+            tmp = tmp.next;
         }
 
-        //两个都不为空，取一个小的节点，同时移动对应的传
-        int val1 = list1.val;
-        int val2 = list2.val;
-        if (val1 <= val2) {
-            result.val = val1;
-            list1 = list1.next;
-            result.next = mergeTwoLists(list1, list2);
+        if (null != list1) {
+            tmp.next = list1;
         } else {
-            result.val = val2;
-            list2 = list2.next;
-            result.next = mergeTwoLists(list1, list2);
+            tmp.next = list2;
         }
-        return result;
+
+        return result.next;
 
     }
 }
