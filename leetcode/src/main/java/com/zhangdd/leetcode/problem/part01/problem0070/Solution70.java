a40cd99fb9c67fe545ffb357442e02af1251ce34
zhangdd <86431843@qq.com>
2023-12-31 16:37:38 +0800

    refactor:70. 爬楼梯
    Signed-off-by: zhangdd <86431843@qq.com>

--- leetcode/src/main/java/com/zhangdd/leetcode/problem/part01/problem0070/Solution70.java
+++ leetcode/src/main/java/com/zhangdd/leetcode/problem/part01/problem0070/Solution70.java
@@ -7,10 +7,13 @@ package com.zhangdd.leetcode.problem.part01.problem0070;
 public class Solution70 {
 
     public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
         int[] dp = new int[n + 1];
-        dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
